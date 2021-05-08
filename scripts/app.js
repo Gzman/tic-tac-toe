ea3e06c3f0f0118ef59d851bfa4401be1a3e5e2a
Gzman <ganzmann@protonmail.com>
2021-05-08 23:59:11 +0200

    Code refactoring and fixed bug when cell clicked which was already marked

--- scripts/app.js
+++ scripts/app.js
@@ -13,32 +13,29 @@ const player2 = Player("Player 2", "O");
 
 const GameBoard = (() => {
     const EMPTY = "";
-    let _cells = new Array(9).fill(EMPTY);
-    const _isCellMarked = (index, marker) => _cells[index] === marker;
-    const reset = () => _cells = _cells.map(cell => cell = EMPTY);
-    const getMark = index => _cells[index];
+    let cells = new Array(9).fill(EMPTY);
+    const isCellMarked = (index, marker) => cells[index] === marker;
+    const reset = () => cells = cells.map(cell => cell = EMPTY);
+    const getMark = index => cells[index];
     const markCell = (index, marker) => {
-        if (_isCellMarked(index, EMPTY)) {
-            _cells[index] = marker;
+        if (isCellMarked(index, EMPTY)) {
+            cells[index] = marker;
         }
     }
-    const areAllCellsMarked = () => _cells.filter(cell => cell !== EMPTY).length === _cells.length;
-    const getAllMarkedIndicies = (mark) => _cells.reduce((markedIndicies, cell, cellIndex) => {
+    const areAllCellsMarked = () => cells.filter(cell => cell !== EMPTY).length === cells.length;
+    const getAllMarkedIndicies = (mark) => cells.reduce((markedIndicies, cell, cellIndex) => {
         if (cell === mark) markedIndicies.push(cellIndex);
         return markedIndicies;
     }, []);
-    const getAllUnmarkedIndicies = () => _cells.reduce((unmarkedIndicies, cell, cellIndex) => {
-        if (cell === EMPTY) unmarkedIndicies.push(cellIndex);
-        return unmarkedIndicies;
-    }, []);
-    const satisfyWinCondition = marker => (_isCellMarked(0, marker) && (_cells[0] === _cells[1] && _cells[0] === _cells[2])) ||
-        (_isCellMarked(3, marker) && (_cells[3] === _cells[4] && _cells[3] === _cells[5])) ||
-        (_isCellMarked(6, marker) && (_cells[6] === _cells[7] && _cells[6] === _cells[8])) ||
-        (_isCellMarked(0, marker) && (_cells[0] === _cells[3] && _cells[0] === _cells[6])) ||
-        (_isCellMarked(1, marker) && (_cells[1] === _cells[4] && _cells[1] === _cells[7])) ||
-        (_isCellMarked(2, marker) && (_cells[2] === _cells[5] && _cells[2] === _cells[8])) ||
-        (_isCellMarked(0, marker) && (_cells[0] === _cells[4] && _cells[0] === _cells[8])) ||
-        (_isCellMarked(2, marker) && (_cells[2] === _cells[4] && _cells[2] === _cells[6]));
+    const getAllUnmarkedIndicies = () => getAllMarkedIndicies(EMPTY);
+    const satisfyWinCondition = marker => (isCellMarked(0, marker) && (cells[0] === cells[1] && cells[0] === cells[2])) ||
+        (isCellMarked(3, marker) && (cells[3] === cells[4] && cells[3] === cells[5])) ||
+        (isCellMarked(6, marker) && (cells[6] === cells[7] && cells[6] === cells[8])) ||
+        (isCellMarked(0, marker) && (cells[0] === cells[3] && cells[0] === cells[6])) ||
+        (isCellMarked(1, marker) && (cells[1] === cells[4] && cells[1] === cells[7])) ||
+        (isCellMarked(2, marker) && (cells[2] === cells[5] && cells[2] === cells[8])) ||
+        (isCellMarked(0, marker) && (cells[0] === cells[4] && cells[0] === cells[8])) ||
+        (isCellMarked(2, marker) && (cells[2] === cells[4] && cells[2] === cells[6]));
     return {
         reset,
         getMark,
@@ -51,21 +48,40 @@ const GameBoard = (() => {
 })();
 
 const DisplayCtrl = (() => {
-    const _init = (() => {
-        document.querySelectorAll(".gameboard-cell").forEach(cell => cell.addEventListener("click", play));
+    const gameboardCells = document.querySelectorAll(".gameboard-cell");
+    const selectOpponentModal = document.querySelector(".select-opponent-layer");
+    const outcomeModal = document.querySelector(".outcome-layer");
+    const play = (event) => {
+        const cellElement = event.currentTarget;
+        if(cellElement.children[0].textContent !== "") return;
+        const selectedIdx = parseInt(cellElement.dataset.index);
+        GameCtrl.play(selectedIdx);
+    }
+    const restart = () => {
+        GameCtrl.reset();
+        outcomeModal.style.display = "none";
+        renderOpponentSelection();
+    }
+    const selectOpponent = (event) => {
+        player2.setAsCpu(JSON.parse(event.currentTarget.dataset.isCpu));
+        player2.setName(player2.isCpu() ? "Computer" : "Player 2");
+        selectOpponentModal.style.display = "none";
+    }
+    const init = (() => {
+        gameboardCells.forEach(cell => cell.addEventListener("click", play));
         document.querySelector(".restart-btn").addEventListener("click", restart);
-        document.querySelector(".outcome-content").addEventListener("click", restart);
-        document.querySelector(".outcome-layer").addEventListener("click", restart);
+        outcomeModal.addEventListener("click", restart);
+        outcomeModal.querySelector(".outcome-content").addEventListener("click", restart);
         document.querySelector(".player-vs-player").addEventListener("click", selectOpponent);
         document.querySelector(".player-vs-computer").addEventListener("click", selectOpponent);
     })();
-    const renderGameboard = () => document.querySelectorAll(".gameboard-cell").forEach(cell => cell.children[0].textContent = GameBoard.getMark([parseInt(cell.dataset.index)]));
+    const renderGameboard = () => gameboardCells.forEach(cell => cell.children[0].textContent = GameBoard.getMark([parseInt(cell.dataset.index)]));
+    const renderOpponentSelection = () => selectOpponentModal.style.display = "block";
     const renderOutcome = (outcomeMsg) => {
-        document.querySelector(".outcome-layer").style.display = "block";
-        document.querySelector(".outcome-message").textContent = outcomeMsg;
+        outcomeModal.style.display = "block";
+        outcomeModal.querySelector(".outcome-message").textContent = outcomeMsg;
     }
     const renderTurn = message => document.querySelector(".current-turn").textContent = message;
-    const renderOpponentSelection = () => document.querySelector(".select-opponent-layer").style.display = "block";
     return { renderGameboard, renderOutcome, renderTurn, renderOpponentSelection };
 })();
 
@@ -75,14 +91,14 @@ const GameCtrl = ((player1, player2) => {
         GameBoard.markCell(index, currentPlayer.getMarker());
         DisplayCtrl.renderGameboard();
         if (hasPlayerWon()) {
-            DisplayCtrl.renderOutcome(`${currentPlayer.getMarker()} wins!`);
+            DisplayCtrl.renderOutcome(`${currentPlayer.getName()} wins!`);
         }
         else if (isDraw()) {
             DisplayCtrl.renderOutcome(`Draw!`);
         }
         else {
             changePlayer();
-            DisplayCtrl.renderTurn(`${currentPlayer.getMarker()}'s turn`);
+            displayCurrentTurn();
         }
     }
     const computerPlay = () => {
@@ -95,39 +111,26 @@ const GameCtrl = ((player1, player2) => {
         if (currentPlayer === player1) {
             currentPlayer = player2;
             if (player2.isCpu() === true) {
-                computerPlay();
+                setTimeout(computerPlay, 380);
             }
         } else {
             currentPlayer = player1;
         }
     }
+    const start = () => {
+        DisplayCtrl.renderOpponentSelection();
+        displayCurrentTurn();
+    }
     const reset = () => {
         GameBoard.reset();
         currentPlayer = player1;
-        DisplayCtrl.renderTurn(`${currentPlayer.getMarker()}'s turn`);
+        displayCurrentTurn();
         DisplayCtrl.renderGameboard();
     }
+    const displayCurrentTurn = () => DisplayCtrl.renderTurn(`${currentPlayer.getName()}'s turn`);
     const hasPlayerWon = () => GameBoard.satisfyWinCondition(currentPlayer.getMarker());
     const isDraw = () => GameBoard.areAllCellsMarked() && !hasPlayerWon(player1) && !hasPlayerWon(player2);
-    return { play, reset }
+    return { start, play, reset }
 })(player1, player2);
 
-function play() {
-    const selectedIdx = parseInt(this.dataset.index);
-    GameCtrl.play(selectedIdx);
-}
-
-function restart(event) {
-    if (this !== event.target) return;
-    GameCtrl.reset();
-    document.querySelector(".outcome-layer").style.display = "none";
-    DisplayCtrl.renderOpponentSelection();
-}
-
-function selectOpponent() {
-    player2.setAsCpu(JSON.parse(this.dataset.isCpu));
-    document.querySelector(".select-opponent-layer").style.display = "none";
-}
-
-DisplayCtrl.renderOpponentSelection();
-DisplayCtrl.renderTurn(`${player1.getMarker()}'s turn.`);
+GameCtrl.start();
